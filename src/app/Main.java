142888706a21018c1905256fdd8d5cad97c2acfc
Leonardo Petta <leonardocps9@gmail.com>
2020-03-13 09:07:01 -0300

    Modificações para recebimento

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,34 +1,27 @@
 package app;
 
-import java.io.ObjectOutputStream;
+import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        try {
-            // Instancia o ServerSocket ouvindo a porta 12345
-            ServerSocket servidor = new ServerSocket(12345);
-            System.out.println("Servidor ouvindo a porta 12345");
-            while (true) {
-                // o método accept() bloqueia a execução até que
-                // o servidor receba um pedido de conexão
-                Socket cliente = servidor.accept();
-                System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
-                ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
-                saida.flush();
-                saida.writeObject(new Date());
-                System.out.println("Enviou");
-                saida.close();
-                System.out.println("Saida fechou");
-                cliente.close();
-                System.out.println("cliente fechou");
-            }
-        } catch (Exception e) {
-            System.out.println("Erro: " + e.getMessage());
-        } finally {
-            
+    public static void main(String[] args) throws IOException {
+        ServerSocket servidor = new ServerSocket(12345);
+        System.out.println("Porta 12345 aberta!");
+
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " +     
+            cliente.getInetAddress().getHostAddress()
+        );
+
+        Scanner s = new Scanner(cliente.getInputStream());
+        while (s.hasNextLine()) {
+            System.out.println(s.nextLine());
         }
+
+        s.close();
+        servidor.close();
+        cliente.close();
     }
 }
