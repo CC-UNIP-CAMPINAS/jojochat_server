7e25ebad680cf447c90602dbc6ef5d6988c9496f
Leonardo Petta do Nascimento <leonardops9@protonmail.com>
2020-03-14 15:21:33 -0300

    MultiThread

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,23 +5,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
-	 public static void main(String[] args) throws IOException {
-	        ServerSocket servidor = new ServerSocket(12345);
-	        System.out.println("Porta 12345 aberta!");
+public class Main extends Thread {
+	public Socket conexao;
 
-	        Socket cliente = servidor.accept();
-	        System.out.println("Nova conexão com o cliente " +     
-	            cliente.getInetAddress().getHostAddress()
-	        );
-
-	        Scanner s = new Scanner(cliente.getInputStream());
-	        while (s.hasNextLine()) { 	
-	            System.out.println(s.nextLine());
-	        }
-
-	        s.close();
-	        servidor.close();
-	        cliente.close();
-	    }
+	public Main(Socket aConexao) {
+		this.conexao = aConexao;
 	}
+
+	public static void main(String[] args) throws IOException {
+		ServerSocket servidor = new ServerSocket(12345);
+		System.out.println("Porta 12345 aberta!");
+		boolean fechamento = true;
+
+		while (fechamento) {
+			Socket cliente = servidor.accept();
+			System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
+			Thread t = new Main(cliente);
+			t.start();
+		}
+		servidor.close();
+	}
+
+	public void run() {
+		try {
+			Scanner s = new Scanner(conexao.getInputStream());
+			while (s.hasNextLine()) {
+				System.out.println(s.nextLine());
+			}
+
+			System.out.println("Usuário saiu");
+
+			s.close();
+			conexao.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
