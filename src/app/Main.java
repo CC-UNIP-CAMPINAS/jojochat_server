ba88198f836a914bb166eb9bb6692ad9fe94a033
Leonardo Petta do Nascimento <leonardops9@protonmail.com>
2020-03-14 21:57:59 -0300

    Implementação de varios baguio

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,50 +1,70 @@
 package app;
 
+import java.io.EOFException;
 import java.io.IOException;
-import java.net.ServerSocket;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
-import java.util.Scanner;
+
+import entities.Connection;
 
 public class Main extends Thread {
 	public Socket conexao;
-	public static ArrayList<Socket> clients = new ArrayList<>();
+	public static ArrayList<Socket> conexoes = new ArrayList<>();
+	public static ArrayList<String> clientesConectados = new ArrayList<>();
 
 	public Main(Socket aConexao) {
 		this.conexao = aConexao;
+
 	}
 
 	public static void main(String[] args) throws IOException {
-		ServerSocket servidor = new ServerSocket(12345);
-		System.out.println("Porta 12345 aberta!");
-		boolean fechamento = true;
+		try (Connection servidor = new Connection(12345)) {
+			boolean fechamento = true;
 
-		while (fechamento) {
-			Socket cliente = servidor.accept();
-			System.out.println("Nova conexão com o cliente " + cliente.getInetAddress().getHostAddress());
-			Thread t = new Main(cliente);
-			t.start();
-			clients.add(cliente);
-			for (Socket cli : clients) {
-				System.out.println("Clientes conectados: " + cli.getInetAddress().getHostAddress());
+			while (fechamento) {
+				Socket cliente = servidor.getConnection();
+				Thread t = new Main(cliente);
+				t.start();
+				conexoes.add(cliente);
 			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		servidor.close();
 	}
 
 	public void run() {
-		try {
-			Scanner s = new Scanner(conexao.getInputStream());
-			while (s.hasNextLine()) {
-				System.out.println(s.nextLine());
+		try (ObjectInputStream objIns = new ObjectInputStream(conexao.getInputStream());
+				ObjectOutputStream objOut = new ObjectOutputStream(conexao.getOutputStream())) {
+
+			for (Socket socket : conexoes) {
+				clientesConectados.add(socket.toString());
+			}
+			objOut.writeObject(clientesConectados);
+
+			String mensagem = null;
+			while (true) {
+				mensagem = (String) objIns.readObject();
+				objOut.writeObject(clientesConectados);
+				System.out.println(mensagem);
 			}
 
-			System.out.println("Usuário saiu");
+		} catch (EOFException | SocketException e) {
+			System.out.println("Usuário saiu: " + conexao.toString());
+			conexoes.remove(conexao);
 
-			s.close();
-			conexao.close();
+			for (Socket socket : conexoes) {
+				clientesConectados.add(socket.toString());
+			}
 
 		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
---
+++ src/entities/Connection.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Connection implements AutoCloseable{
+	private ServerSocket servidor;
+	private int porta;
+	
+	public Connection(int port) {
+		try {
+			this.servidor = new ServerSocket(port);
+			this.porta = port;
+			System.out.println("Servidor inicializado na porta: " + port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public ServerSocket getServidor() {
+		return servidor;
+	}
+	
+	public int getPorta() {
+		return porta;
+	}
+	
+	public Socket getConnection() {
+		try {
+			return getServidor().accept();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Nova conexão com o cliente " + getServidor().getInetAddress().getHostAddress());
+		return null;
+	}
+
+	@Override
+	public void close() throws Exception {
+		servidor.close();	
+	}
+
+	
+}
